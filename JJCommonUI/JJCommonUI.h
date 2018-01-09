ff129ba373b6c445d88920f9e88e47c6187a262f
JJ <gongjian_001@126.com>
2018-01-09 10:21:03 +0800

    up

---
+++ JJCommonUI/JJCommonUI.h
@@ -0,0 +1,19 @@
+//
+//  JJCommonUI.h
+//  JJCommonUI
+//
+//  Created by 宫健(金融壹账通客户端研发团队) on 09/01/2018.
+//  Copyright © 2018 宫健(金融壹账通客户端研发团队). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JJCommonUI.
+FOUNDATION_EXPORT double JJCommonUIVersionNumber;
+
+//! Project version string for JJCommonUI.
+FOUNDATION_EXPORT const unsigned char JJCommonUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JJCommonUI/PublicHeader.h>
+
+
